708d84766670474ad8e23dbb1048e2d69199942e
aveencoder <aveen598@gmail.com>
2025-11-16 14:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function Employee(name, age, jobTitle) {
 	Person.call(this,name,age);
 	this.jobTitle = jobTitle;
     this.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	};
 }
 
